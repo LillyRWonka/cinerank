6071dd465a3dd69cb4bd9b01f85f2b05cbaf6b35
Kristina Alabastro <kristinaa7@live.com>
2022-12-21 20:48:33 -0800

    connected go button to IMDb Api

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,34 @@
-var IMDbreviewEl = document.getElementById("IMDb-review");
+var inputSearch = document.getElementById("input");
+var goButton = document.getElementById("go-button");
+var title = ""
+var movie = inputSearch.value
 
-function renderReview () {
-    fetch (`https://imdb-api.com/en/API/Reviews/k_f2lcoitr/tt1375666`)
-    .then(resp=>{
-        return resp.json();
+function renderTitle() {
+    fetch (`https://imdb-api.com/en/API/SearchMovie/k_f2lcoitr/${title}`)
+    .then(response=>{
+        return response.json();
     })
-    .then(data=>{
-        console.log(data);
-    })
+    .then(data =>
+        console.log(data));
+    // .catch(error => console.log(error))
+console.log(title);
+console.log(movie);
 }
+goButton.addEventListener("click", renderTitle);
+renderTitle ();
 
-renderReview ();
+
+//stores the string in the variable, completes a fetch request with the search result
+//when go is clicked, complete a fetch request and console log the movie title
+
+renderTitle ();
+
+// goButton.addEventListener ("click", projectMovie);
+
+// function projectMovie() {
+// // when button is clicked, IMDb movie title + review + Youtube API will load on secondary page
+//     fetch (`https://imdb-api.com/en/API/SearchMovie/k_f2lcoitr/inception 2010`);
+//     //add .then code 
+//     fetch (`https://www.youtube.com/iframe_api`);
+//     //
+// }
--- assets/secondscript.js
+++ assets/secondscript.js
@@ -1,7 +1,8 @@
 var IMDbreviewEl = document.getElementById("IMDb-review");
+var YTplayer = document.getElementById("videoplayer");
 
 function renderReview () {
-    fetch (`https://imdb-api.com/en/API/Reviews/k_f2lcoitr/tt1375666`)
+    fetch (`https://imdb-api.com/en/API/Reviews/k_f2lcoitr`)
     .then(resp=>{
         return resp.json();
     })
@@ -11,3 +12,4 @@ function renderReview () {
 }
 
 renderReview ();
+
